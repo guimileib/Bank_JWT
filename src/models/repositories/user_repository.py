24a2a1aea8a5b9113faacbb65dfd67f2cea4ac02
guimileib <guilhermemileib@gmail.com>
2025-02-13 09:47:17 -0300

    feat: Implementing user repository with connection (need tests)

--- src/models/repositories/user_repository.py
+++ src/models/repositories/user_repository.py
@@ -7,8 +7,35 @@ class UserRepository:
     def registry_user(self, username: str, password: str) -> None:
         cursor = self.__conn.cursor()
         cursor.execute(
-            "INSERT INTO users (username, password, balance) VALUES (?, ?, ?)", 
-            (username, password, 0)
+            '''
+            INSERT INTO users 
+                (username, password, balance) 
+            VALUES 
+                (?, ?, ?); 
+            ''',(username, password, 0)
         )
         self.__conn.commit()
+    
+    def edit_balance(self, user_id: int, new_balance: float) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            UPDATE users
+            SET balance = ?
+            WHERE id = ?
+            ''', (new_balance, user_id)
+        )
+        self.__conn.commit()
+    
+    def get_user_by_username(self, username: str):
+        cursor = self.__conn.cursor()
+        cursor.execute(
+           '''
+           SELECT id, username, password
+           FROM users
+           WHERE username = ?
+           ''', (username,)
+        )
+        user = cursor.fetchone()
+        return user
         
--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,12 +1,21 @@
 from .user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
 
-def test_resgistry_user():
+def test_repository():
     db_connection_handler.connect()
     conn = db_connection_handler.get_connection()
     repo = UserRepository(conn)
     
-    username = "barney"
+    username = "Bob Esponja"
     password = "123Rocket"
     
     repo.registry_user(username, password)
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)
+   
+    repo.edit_balance(user[0], 8989.90)
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)
+    
