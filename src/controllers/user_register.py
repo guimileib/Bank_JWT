71e325d79989f0bb42b26757eeec2bb8e91cad6e
guimileib <guilhermemileib@gmail.com>
2025-02-18 16:27:06 -0300

    feat: Implementing UserRegister

--- src/controllers/user_register.py
+++ src/controllers/user_register.py
@@ -1,6 +1,31 @@
+from typing import Dict
 from src.models.interface.user_repository import UserRepositoryInterface
+from src.drivers.password_handler import PasswordHandler
 
 class UserRegister:
     def __init__(self, user_repository: UserRepositoryInterface) -> None: 
-        pass
+        self.__user_repository = user_repository
+        self.__password_handle = PasswordHandler()
     
+    # Método público para registrar um novo usuário
+    def registry(self, username: str, password: str) -> Dict:
+        hashed_password = self.__create_hash_password(password)
+        self.__registry_new_user(username, hashed_password)
+        return self.__format_response(username)
+        
+    # Cria a senha criptografada
+    def __create_hash_password(self, password: str) -> str:
+        hash_password = self.__password_handle.encrypt_password(password)
+        return hash_password
+    
+    # Faço o registro do usuário
+    def __registry_new_user(self, username: str, hashed_password: str) -> None:
+        self.__user_repository.registry_user(username, hashed_password)
+    
+    # Lugar a o retorno da informação 
+    def __format_response(self, username: str) -> Dict:
+        return {
+            "type": "User",
+            "count": 1,
+            "username": username
+        }
---
+++ src/controllers/user_register_test.py
@@ -0,0 +1,26 @@
+from .user_register import UserRegister
+
+# Mock imita o Repositorio do usuário
+class MockUserRepository:
+    def __init__(self) -> None:
+        self.registry_user_attributtes = {}
+
+    def registry_user(self, username, password) -> None:
+        self.registry_user_attributtes["username"] = username
+        self.registry_user_attributtes["password"] = password
+
+def test_registry():
+    repository = MockUserRepository()
+    controller = UserRegister(repository)
+    
+    username = "olaMundo"
+    password = "myPassword"
+    
+    response = controller.registry(username, password)
+
+    assert response["type"] == "User"
+    assert response["username"] == username
+    
+    assert repository.registry_user_attributter["username"] == username
+    assert repository.registry_user_attributter["password"] is not None
+    assert repository.registry_user_attributter["password"] != password
