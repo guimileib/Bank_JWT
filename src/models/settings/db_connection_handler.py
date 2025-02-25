cc4d6e309c8ab9903f666b6fe9e83d681a374dbc
guimileib <guilhermemileib@gmail.com>
2025-02-25 20:10:16 -0300

    feat: Implementing routes with flask

--- src/models/settings/db_connection_handler.py
+++ src/models/settings/db_connection_handler.py
@@ -6,8 +6,11 @@ class __DBConncetionHandler:
         self.__connection_string = "storage.db"
         self.__conn = None
         
-    def connect(self):
-        self.__conn = sqlite3.connect(self.__connection_string)
+    def connect(self) -> None:
+        self.__conn = sqlite3.connect(
+            self.__connection_string, 
+            check_same_thread=False
+        )
         
     def get_connection(self) -> Connection:
         return self.__conn
