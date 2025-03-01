bd695b44835e836cb886945c3d5d0fd66575cbca
guimileib <guilhermemileib@gmail.com>
2025-03-01 16:26:50 -0300

    feat: Implementing middleware with auth_verify

---
+++ src/main/middlewares/auth_jwt.py
@@ -0,0 +1,19 @@
+from flask import request
+from src.drivers.jwt_handler import JWTHandler
+
+def auth_jwt_verify():
+    jwt_handle = JWTHandler()
+    raw_token = request.headers.get("Authorization") # Pega o token que está no header da requisição que está chegando no servidor
+    user_id = request.headers.get("uid")
+    
+    if not raw_token or not user_id:
+        raise Exception("Invalid Auth information")
+    
+    token = raw_token.split()[1] # Pega o token que está no header da requisição que está chegando no servidor
+    token_information = jwt_handle.decode_jwt_token(token) # Decodifica o token, tira as informações que estão dentro do token
+    token_uid = token_information["user_id"]
+    
+    if user_id and token_uid and (int(token_uid)) == int(user_id):
+        return token_information
+    
+    raise Exception("User Unaunthorized")
--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -5,6 +5,8 @@ from src.main.composer.user_register_composer import user_register_composer
 from src.main.composer.login_creator_composer import login_creator_composer
 from src.main.composer.balance_editor import balance_editor_composer
 
+from src.main.middlewares.auth_jwt import auth_jwt_verify
+
 bank_routes_bp = Blueprint("bank_routes", __name__)
 
 @bank_routes_bp.route("/bank/registry", methods=["POST"])
@@ -21,7 +23,13 @@ def create_login():
 
 @bank_routes_bp.route("/bank/balance/<user_id>", methods=["PATCH"]) # PATCH = quando você quer atualizar um recurso dentro do servidor
 def edit_balance(user_id):
-    http_request = HttpRequest(body=request.json, params={"user_id": user_id})
+    token_information = auth_jwt_verify()
+    http_request = HttpRequest(
+        body=request.json, 
+        params={"user_id": user_id},
+        token_infos=token_information # Passando o token_information para o HttpRequest
+        )
+    
     http_response = balance_editor_composer().handle(http_request)
     return jsonify(http_response.body), http_response.status_code
     
